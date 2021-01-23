63dce92ff46ebfb2edae352306e9657da94f3c3c
Anton Sakhniuk <anton.sakhniuk@gmail.com>
2021-01-24 01:08:54 +0200

    Created BinaryFlag.Test project. Added tests to cover MultipleBinaryFlag class using White box method

---
+++ BinaryFlag.Test/MultipleBinaryFlagTest.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using IIG.BinaryFlag;
+using System.Collections.Generic;
+
+namespace BinaryFlag.Test
+{
+	[TestClass]
+	public class MultipleBinaryFlagTest
+	{
+		const ulong MAX_ALLOWED_SIZE = 17179868704;
+
+		[TestMethod]
+		public void Constructor_Throws_ArgumentOutOfRange_LengthLessThan2()
+		{
+			Action test_length_1 = () => { new MultipleBinaryFlag(1); };
+			Action test_length_0 = () => { new MultipleBinaryFlag(0); };
+			Assert.ThrowsException<ArgumentOutOfRangeException>(test_length_1);
+			Assert.ThrowsException<ArgumentOutOfRangeException>(test_length_0);
+		}
+
+
+		[TestMethod]
+		public void Constructor_Throws_ArgumentOutOfRange_LengthMoreThanMax()
+		{
+			Action test_length_out_of_range = () => { new MultipleBinaryFlag(MAX_ALLOWED_SIZE+1); };
+			Assert.ThrowsException<ArgumentOutOfRangeException>(test_length_out_of_range);
+		}
+
+		[TestMethod]
+		public void Test_UIntConcreteBinaryFlag_Implementation()
+		{
+			// UIntConcreteBinaryFlag implementation is used when 2<=length<=32. 
+			// We'll test lowest and highest length from this range
+			var sizes_to_test = new List<ulong>() { 2, 32 };
+			sizes_to_test.ForEach((len) => {
+				TestFullFlow(len, initialValue: true);
+				TestFullFlow(len, initialValue: false);
+			});
+		}
+
+		[TestMethod]
+		public void Test_ULongConcreteBinaryFlag_Implementation()
+		{
+			var sizes_to_test = new List<ulong>() { 33, 64 };
+			sizes_to_test.ForEach((len) => {
+				TestFullFlow(len, initialValue: true);
+				TestFullFlow(len, initialValue: false);
+			});
+		}
+
+		[TestMethod]
+		public void Test_UIntArrayConcreteBinaryFlag_Implementation()
+		{
+			var sizes_to_test = new List<ulong>() { 65, 4096 };
+			sizes_to_test.ForEach((len) => {
+				TestFullFlow(len, initialValue: true);
+				TestFullFlow(len, initialValue: false);
+			});
+		}
+
+		/// <summary>
+		/// Runs full test of MultipleBinaryFlag given specified length and initialValue.
+		/// Consists of such steps:
+		/// 1. Test that new object is setting up initial value correctly by checking GetFlag() and ToString() method's return values.
+		/// 2. Test ResetFlag() works as intended by resetting flag at each position of 0..length-1. Checks expected output of GetFlag() and ToString() on each step.
+		/// 3. Test SetFlag() similar to p.2 (ResetFlag())
+		/// 4. Test ArgumentOutOfRangeException is being thrown when calling ResetFlag() or SetFlag() with position>=length
+		/// </summary>
+		/// <param name="len"></param>
+		/// <param name="initialValue"></param>
+		private static void TestFullFlow(ulong length, bool initialValue)
+		{
+			MultipleBinaryFlag impl = new MultipleBinaryFlag(length, initialValue);
+
+			// 1. Test that new object is setting up initial value correctly.
+			char expected_char = initialValue ? 'T' : 'F';
+			string expected = new string(expected_char, (int)length);
+			char[] expected_arr = expected.ToCharArray();
+			Assert.AreEqual(initialValue, impl.GetFlag()); // GetFlag should be equal to initialValue right after object construction
+
+			var actual = impl.ToString();
+			Assert.AreEqual<string>(expected, actual, $"Expected string of '{expected_char}' chars of {length} symbols. Got '{actual}' instead");
+
+			// 2. Test ResetFlag(): apply reset for each element by one, compare output (ToString) to expected. 
+			// Also checks if GetFlag() returns false (since not all elements are True)
+			for (ulong i = 0; i < length; ++i)
+			{
+				impl.ResetFlag(i);
+				expected_arr[i] = 'F';
+				expected = new string(expected_arr);
+
+				actual = impl.ToString();
+				Assert.AreEqual<string>(expected, actual);
+				Assert.IsFalse(impl.GetFlag());
+			}
+
+			// 3. Test SetFlag(): set flag for each position by one, compare output (ToString) to expected. 
+			// Also checks if GetFlag() returns false (since not all elements are True)
+			for (ulong i = 0; i < length; ++i)
+			{
+				impl.SetFlag(i);
+				expected_arr[i] = 'T';
+				expected = new string(expected_arr);
+
+				actual = impl.ToString();
+				Assert.AreEqual<string>(expected, actual);
+				// After last step (i==len-1) flag should become true.
+				// Before that it should remain false, as not all flags are true.
+				if (i < length - 1)
+					Assert.IsFalse(impl.GetFlag());
+				else Assert.IsTrue(impl.GetFlag());
+			}
+
+			// 4. Test ArgumentOutOfRangeException is being thrown when calling ResetFlag/SetFlag with position>=length
+			Action throws_outOfRange_resetFlag = () => { impl.ResetFlag(length); };
+			Action throws_outOfRange_setFlag = () => { impl.SetFlag(length); };
+			Assert.ThrowsException<ArgumentOutOfRangeException>(throws_outOfRange_setFlag);
+			Assert.ThrowsException<ArgumentOutOfRangeException>(throws_outOfRange_resetFlag);
+
+			impl.Dispose();
+			impl.Dispose(); // Verify object handles second disposal request without errors
+		}
+	}
+}
---
+++ BinaryFlag.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BinaryFlag.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BinaryFlag.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ec57dab-9197-46a3-8644-4f7f01b235e7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
